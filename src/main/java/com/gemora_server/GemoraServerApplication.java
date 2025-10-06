60b9a0f176d8f8c7ea71832698f67625243c2e40
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-10-06 21:00:23 +0530

    Create springboot application with required dependancies.

---
+++ src/main/java/com/gemora_server/GemoraServerApplication.java
@@ -0,0 +1,13 @@
+package com.gemora_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemoraServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemoraServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemora_server/GemoraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemora_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemoraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
