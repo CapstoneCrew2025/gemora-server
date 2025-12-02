4e2e61a521b15573eef648248ed24a886647cb30
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-02 10:30:00 +0530

    profile udpade dto update

--- src/main/java/com/gemora_server/dto/ProfileUpdateDto.java
+++ src/main/java/com/gemora_server/dto/ProfileUpdateDto.java
@@ -7,9 +7,18 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+// 👉 Automatically generates:
+//    - Getters
+//    - Setters
+//    - toString()
+//    - equals()
+//    - hashCode()
+// So you don’t need to write them manually.
 @Builder
+
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class ProfileUpdateDto {
     private String name;
     private String contactNumber;
