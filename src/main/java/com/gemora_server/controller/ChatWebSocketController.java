a45dbf29a9c2641e5ac7af86019519980ccd6b1b
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-12-09 13:06:02 +0530

    issuefix all issues .

--- src/main/java/com/gemora_server/controller/ChatWebSocketController.java
+++ src/main/java/com/gemora_server/controller/ChatWebSocketController.java
@@ -1,21 +1,14 @@
 package com.gemora_server.controller;
 
-
 import com.gemora_server.dto.ChatMessageRequestDto;
 import com.gemora_server.dto.ChatMessageResponseDto;
 import com.gemora_server.service.ChatMessageService;
 import com.gemora_server.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
